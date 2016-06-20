fcb58cd65b07607d358b96c6ac4764de33d5943b
andy <djadub3@gmail.com>
2016-06-20 01:29:56 -0700

    add graph type listView

--- app/src/main/java/com/example/andy/solenergi/MainActivity.java
+++ app/src/main/java/com/example/andy/solenergi/MainActivity.java
@@ -2,14 +2,35 @@ package com.example.andy.solenergi;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    String[] graphTypes={"live View","Day View","Week View"};
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_solenergi);
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, graphTypes);
+        ListView graphListView = (ListView) findViewById(R.id.graphList);
+        graphListView.setAdapter(adapter);
+
+        graphListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Log.v("clickListener test ", "clicked");
+                String graphType = adapter.getItem(position);
+                Log.v("grph list", graphType + " was clicked");
+
+            }
+        });
     }
 
 
