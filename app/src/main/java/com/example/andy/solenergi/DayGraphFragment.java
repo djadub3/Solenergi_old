d43e926d8c3e2d23f97278930771ad36c5cdf1e0
andy <djadub3@gmail.com>
2016-06-27 22:47:06 -0700

    day graph with correct times

--- app/src/main/java/com/example/andy/solenergi/DayGraphFragment.java
+++ app/src/main/java/com/example/andy/solenergi/DayGraphFragment.java
@@ -1,6 +1,5 @@
 package com.example.andy.solenergi;
 
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -22,15 +21,6 @@ import java.util.GregorianCalendar;
 import java.util.Locale;
 import java.util.TimeZone;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link DayGraphFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link DayGraphFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class DayGraphFragment extends Fragment {
 
 
@@ -52,34 +42,6 @@ public class DayGraphFragment extends Fragment {
     private LineGraphSeries<DataPoint> series2;
     private LineGraphSeries<DataPoint> series3;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DayGraphFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DayGraphFragment newInstance(String param1, String param2) {
-        DayGraphFragment fragment = new DayGraphFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     public DayGraphFragment() {
         // Required empty public constructor
@@ -105,41 +67,6 @@ public class DayGraphFragment extends Fragment {
 
         super.onViewCreated(view, savedInstanceState);
         mView =view;
-        /*
-        GraphView graph = (GraphView) view.findViewById(R.id.day_graph);
-        series = new LineGraphSeries<DataPoint>();
-        graph.addSeries(series);
-        graph.getGridLabelRenderer().setGridColor(Color.GRAY);
-        series2 = new LineGraphSeries<DataPoint>();
-        graph.addSeries(series2);
-        series3 = new LineGraphSeries<DataPoint>();
-        graph.getSecondScale().addSeries(series3);
-
-        graph.getGridLabelRenderer().setVerticalLabelsSecondScaleColor(Color.GRAY);
-        graph.getGridLabelRenderer().setVerticalLabelsColor(Color.GRAY);
-
-        graph.getSecondScale().setMinY(0);
-        graph.getSecondScale().setMaxY(30);
-
-        series2.setColor(Color.YELLOW);
-        series.setColor(Color.MAGENTA);
-        series3.setColor(Color.CYAN);
-
-        //graph.getViewport().setXAxisBoundsManual(true);
-        graph.getViewport().setYAxisBoundsManual(true);
-        //graph.getViewport().setMinX(0);
-        //graph.getViewport().setMaxX(86400);
-        graph.getViewport().setMinY(0);
-        graph.getViewport().setMaxY(15);
-        //graph.getViewport().setScrollable(true);
-        graph.getGridLabelRenderer().setPadding(30);
-        graph.getGridLabelRenderer().setHorizontalLabelsVisible(false);
-        graph.getGridLabelRenderer().setNumVerticalLabels(16);
-        graph.getGridLabelRenderer().setVerticalAxisTitle("Volts/Amps");
-        //graph.getGridLabelRenderer().set
-        graph.getGridLabelRenderer().setVerticalAxisTitleColor(Color.GRAY);
-        graph.getGridLabelRenderer().setVerticalLabelsSecondScaleColor(Color.GRAY);
-        graph.getGridLabelRenderer().reloadStyles();*/
     }
 
     public void updateGraph(JSONObject inputJson)
@@ -152,6 +79,7 @@ public class DayGraphFragment extends Fragment {
                 voltage = Double.parseDouble(inputJson.getString("involtage"));
                 batVoltage = Double.parseDouble(inputJson.getString("batVoltage"));
                 current = Math.abs(Double.parseDouble(inputJson.getString("current")));
+
                 int year = Integer.parseInt(time.substring(16,20));
                 int month = Integer.parseInt(time.substring(13,15));
                 int day = Integer.parseInt(time.substring(10,12));
@@ -160,8 +88,9 @@ public class DayGraphFragment extends Fragment {
                 int sec = Integer.parseInt(time.substring(4,6));
 
                 Calendar cal=new GregorianCalendar(year,month,day,hour,min,sec);
+                cal.setTimeZone(TimeZone.getTimeZone("Etc/GMT"));
+                Log.v("Time", String.valueOf(cal.getTimeZone()));
                 dataPointArrayList.add(new DataPoint(cal.getTime(), voltage));
-
 
             }
 
@@ -178,7 +107,7 @@ public class DayGraphFragment extends Fragment {
                         // TODO Auto-generated method stub
                         if (isValueX) {
                             SimpleDateFormat sdf = new SimpleDateFormat("h:mm a", Locale.ENGLISH);
-                            sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+                            sdf.setTimeZone(TimeZone.getDefault());
                             String formattedDate = sdf.format(value);
                             return formattedDate;
                         }
@@ -186,41 +115,11 @@ public class DayGraphFragment extends Fragment {
                     }
                 });
                 dataPointArrayList = new ArrayList<DataPoint>();
-
-
             }
 
         }catch(Exception e){
             Log.v("DAY_VIEW","graph not updated");
         }
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
     }
 
 }
